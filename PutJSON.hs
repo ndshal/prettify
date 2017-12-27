dc4f1d918834954b7462c6033f584dd5b1e92c36
Nikita Shalimov <ndshalimov@gmail.com>
2017-12-27 10:16:00 -0500

    Fix typos in PutJSON

--- PutJSON.hs
+++ PutJSON.hs
@@ -5,7 +5,7 @@ import SimpleJSON
 
 renderJValue :: JValue -> String
 
-renderJValue (JString)     = show s
+renderJValue (JString s)     = show s
 renderJValue (JNumber n)   = show n
 renderJValue (JBool True)  = "true"
 renderJValue (JBool False) = "false"
@@ -16,9 +16,9 @@ renderJValue (JObject o) = "{" ++ pairs o ++ "}"
           pairs ps = intercalate ", " (map renderPair ps)
           renderPair (k, v) = show k ++ ": " ++ renderJValue v
 
-renderJValue (JArray a) = "[" ++ values ++ "]"
+renderJValue (JArray a) = "[" ++ values a ++ "]"
     where values [] = ""
           values vs = intercalate ", " (map renderJValue vs)
 
 putJValue :: JValue -> IO ()
-putJValue = ptStrLine . renderJValue
+putJValue = putStrLn . renderJValue
