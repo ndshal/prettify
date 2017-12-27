90922b059e0d431229cf664071a7d7e52486652f
Nikita Shalimov <ndshalimov@gmail.com>
2017-12-27 10:11:22 -0500

    Add renderJValue (JArray) to PutJSON

--- PutJSON.hs
+++ PutJSON.hs
@@ -15,3 +15,7 @@ renderJValue (JObject o) = "{" ++ pairs o ++ "}"
     where pairs [] = ""
           pairs ps = intercalate ", " (map renderPair ps)
           renderPair (k, v) = show k ++ ": " ++ renderJValue v
+
+renderJValue (JArray a) = "[" ++ values ++ "]"
+    where values [] = ""
+          values vs = intercalate ", " (map renderJValue vs)
