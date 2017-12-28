6819cab119d833d75d7be56ceb9e381f2bd2dda7
Nikita Shalimov <ndshalimov@gmail.com>
2017-12-27 19:44:21 -0500

    Implement <> and hcat

--- src/Prettify.hs
+++ src/Prettify.hs
@@ -28,13 +28,20 @@ line :: Doc
 line = Line
 
 (<>) :: Doc -> Doc -> Doc
-a <> b = undefined
+x <> Empty = x
+Empty <> y = y
+x <> y     = x `Concat` y
 
 hcat :: [Doc] -> Doc
-hcat xs = undefined
+hcat = fold (<>)
 
 fsep :: [Doc] -> Doc
 fsep xs = undefined
+
+-- to build a fold exclusively for Doc, note that Empty is the
+-- identity for concatenation.
+fold :: (Doc -> Doc -> Doc) -> [Doc] -> Doc
+fold f = foldr f empty
 
 compact :: Doc -> String
 compact x = undefined
