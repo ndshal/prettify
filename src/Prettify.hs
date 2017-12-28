3f8c76f9453a52e1497131bb630580d7b689f5e8
Nikita Shalimov <ndshalimov@gmail.com>
2017-12-27 19:52:03 -0500

    Implement </> and fsep, along with relevant helper methods

--- src/Prettify.hs
+++ src/Prettify.hs
@@ -27,6 +27,23 @@ double num = text (show num)
 line :: Doc
 line = Line
 
+-- softline should insert a Line [break] if the document is too wide,
+-- and otherwise a space. The way to do this is to keep a Union of
+-- two potential states.
+softline :: Doc
+softline = group line
+
+group :: Doc -> Doc
+group x = flatten x `Union` x
+
+-- note that flatten always gets called on the left element of a Union:
+-- the left subtree of a Union is always at least as long as the right
+flatten :: Doc -> Doc
+flatten (x `Concat` y) = flatten x `Concat` flatten y
+flatten Line           = Char ' '
+flatten (x `Union` _)  = flatten x
+flatten other          = other
+
 (<>) :: Doc -> Doc -> Doc
 x <> Empty = x
 Empty <> y = y
@@ -35,8 +52,11 @@ x <> y     = x `Concat` y
 hcat :: [Doc] -> Doc
 hcat = fold (<>)
 
+(</>) :: Doc -> Doc -> Doc
+x </> y = x <> softline <> y
+
 fsep :: [Doc] -> Doc
-fsep xs = undefined
+fsep = fold (</>)
 
 -- to build a fold exclusively for Doc, note that Empty is the
 -- identity for concatenation.
