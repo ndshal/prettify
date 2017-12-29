114becdab9bfd24aa6597957644159e73c4269ec
Nikita Shalimov <ndshalimov@gmail.com>
2017-12-28 20:31:29 -0500

    Fix indentation in Prettify

--- src/Prettify.hs
+++ src/Prettify.hs
@@ -81,19 +81,19 @@ compact x = transform [x]
 pretty :: Int -> Doc -> String
 pretty width x = best 0 [x]
     where best col (d:ds) =
-        case d of
-            Empty        -> best col ds
-            Char c       -> c : best (col + 1) ds
-            Text s       -> s ++ best (col + length s) ds
-            Line         -> '\n' : best 0 ds
-            a `Concat` b -> best col (a:b:ds)
-            a `Union` b  -> nicest (best col (a:ds))
-                                   (best col (b:ds))
+              case d of
+                  Empty        -> best col ds
+                  Char c       -> c : best (col + 1) ds
+                  Text s       -> s ++ best (col + length s) ds
+                  Line         -> '\n' : best 0 ds
+                  a `Concat` b -> best col (a:b:ds)
+                  a `Union` b  -> nicest col (best col (a:ds))
+                                             (best col (b:ds))
           best _ _ = ""
 
-          nicest col a b = | (width - least) `fits` a = a
-                           | otherwise                = b
-                           where least = min width col
+          nicest col a b | (width - least) `fits` a = a
+                         | otherwise                = b
+                         where least = min width col
 
 -- helper method to determine whether a given number of columns
 -- fits a string
